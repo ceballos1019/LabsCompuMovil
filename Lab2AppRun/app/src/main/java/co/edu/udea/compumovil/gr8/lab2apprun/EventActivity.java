3dd306f05e87faf4da08cf80b44cbe66839982e5
Andres Ceballos Sanchez <Andres Ceballos Sanchez>
2016-03-17 23:09:33 -0500

    Avance en lista de eventos

--- Lab2AppRun/app/src/main/java/co/edu/udea/compumovil/gr8/lab2apprun/EventActivity.java
+++ Lab2AppRun/app/src/main/java/co/edu/udea/compumovil/gr8/lab2apprun/EventActivity.java
@@ -1,15 +1,66 @@
 package co.edu.udea.compumovil.gr8.lab2apprun;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by Sergio on 06/03/2016.
  */
-public class EventActivity extends AppCompatActivity {
+public class EventActivity extends AppCompatActivity implements EventListFragment.OnHeadlineSelectedListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.carreras);
+        setTitle("Eventos");
+        setContentView(R.layout.events);
+
+        if(findViewById(R.id.fragment_container) != null){
+
+            Fragment eventListFragment = new EventListFragment();
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.add(R.id.fragment_container, eventListFragment);
+            transaction.commit();
+
+            //getSupportFragmentManager().beginTransaction()
+            //        .add(R.id.fragment_container, headlinesFragment).commit();
+
+        }
+
+    }
+
+    public void onArticleSelected(int position) {
+        // The user selected the headline of an article from the HeadlinesFragment
+
+        // Capture the article fragment from the activity layout
+        /*EventDetailsFragment articleFrag = (EventDetailsFragment)
+                getSupportFragmentManager().findFragmentById(R.id.article_fragment);
+
+        if (articleFrag != null) {
+            // If article frag is available, we're in two-pane layout...
+
+            // Call a method in the ArticleFragment to update its content
+            articleFrag.updateArticleView(position);
+
+        } else {
+            // If the frag is not available, we're in the one-pane layout and must swap frags...
+
+            // Create fragment and give it an argument for the selected article
+            */
+            EventDetailsFragment newFragment = new EventDetailsFragment();
+            Bundle args = new Bundle();
+            args.putInt(EventDetailsFragment.ARG_POSITION, position);
+            newFragment.setArguments(args);
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+            // Replace whatever is in the fragment_container view with this fragment,
+            // and add the transaction to the back stack so the user can navigate back
+            transaction.replace(R.id.fragment_container, newFragment);
+            transaction.addToBackStack(null);
+
+            // Commit the transaction
+            transaction.commit();
+        //}
+
     }
 }
---
+++ Lab2AppRun/app/src/main/java/co/edu/udea/compumovil/gr8/lab2apprun/Events.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package co.edu.udea.compumovil.gr8.lab2apprun;
+
+public class Events {
+
+    static String[] Headlines = {
+        "Carrera de Barcelona - 20km - 06/03/2016",
+        "Carrera de Paris - 50km - 12/03/2016",
+        "Carrera de Brasil - 120km - 16/08/2016"
+
+    };
+
+    static String[] Articles = {
+            "Carrera de Barcelona - 20km - 06/03/2016\n\n" +
+                    "Descripcion: Es una carrera muy divertida, es mi favorita." +
+                    "\nDistancia: 20km" +
+                    "\nLugar: por mi casa" +
+                    "\nFecha: 06/03/2016. Inscripciones hasta mañana a la 1 de la tarde " +
+                    "\nDatos de contacto: andresrxn10@hotmail.com - 3151511515",
+
+            "Carrera de Barcelona - 20km - 06/03/2016 \n\n"+
+                    "Descripcion: Es una carrera muy divertida, es mi favorita." +
+                    "\nDistancia: 20km" +
+                    "\nLugar: por mi casa" +
+                    "\nFecha: 06/03/2016. Inscripciones hasta mañana a la 1 de la tarde " +
+                    "\nDatos de contacto: andresrxn10@hotmail.com - 3151511515"
+    };
+}
