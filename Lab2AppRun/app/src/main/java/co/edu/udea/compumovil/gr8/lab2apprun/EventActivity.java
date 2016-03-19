a34d2edf5f10725474c9111d5b20e2e7a602c14f
Andres Ceballos Sanchez <Andres Ceballos Sanchez>
2016-03-19 13:31:37 -0500

    Navigation drawer funcionando

--- Lab2AppRun/app/src/main/java/co/edu/udea/compumovil/gr8/lab2apprun/EventActivity.java
+++ Lab2AppRun/app/src/main/java/co/edu/udea/compumovil/gr8/lab2apprun/EventActivity.java
@@ -2,38 +2,120 @@ package co.edu.udea.compumovil.gr8.lab2apprun;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Created by Sergio on 06/03/2016.
  */
-public class EventActivity extends AppCompatActivity implements EventListFragment.OnHeadlineSelectedListener {
+public class EventActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, EventListFragment.OnHeadlineSelectedListener {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("Eventos");
         setContentView(R.layout.events);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        android.support.v7.app.ActionBarDrawerToggle toggle = new android.support.v7.app.ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_event);
         if(findViewById(R.id.fragment_container) != null){
 
-            Fragment eventListFragment = new EventListFragment();
+            Fragment headlinesFragment = new EventListFragment();
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            transaction.add(R.id.fragment_container, eventListFragment);
+            transaction.add(R.id.fragment_container, headlinesFragment);
             transaction.commit();
-
-            //getSupportFragmentManager().beginTransaction()
-            //        .add(R.id.fragment_container, headlinesFragment).commit();
-
         }
+
 
     }
 
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        Fragment fragment=null;
+
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.nav_event:
+                fragment = new EventListFragment();
+                break;
+            case R.id.nav_perfil:
+                fragment = new PerfilFragment();
+                break;
+            case R.id.nav_about:
+                fragment = new AboutFragment();
+                break;
+            default:
+                break;
+        }
+
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        //indicamos que se va comenzar una transaccion para colocar dentro del contenedor de frame el Fragment
+        // que nosotros queremos. y usamos el metodo commit para actualizar el estado
+        fragmentManager.beginTransaction().replace(R.id.fragment_container,fragment).commit();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        /*int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }*/
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public void onArticleSelected(int position) {
         // The user selected the headline of an article from the HeadlinesFragment
 
         // Capture the article fragment from the activity layout
-        /*EventDetailsFragment articleFrag = (EventDetailsFragment)
+        /*ArticleFragment articleFrag = (ArticleFragment)
                 getSupportFragmentManager().findFragmentById(R.id.article_fragment);
 
         if (articleFrag != null) {
@@ -47,7 +129,7 @@ public class EventActivity extends AppCompatActivity implements EventListFragmen
 
             // Create fragment and give it an argument for the selected article
             */
-            EventDetailsFragment newFragment = new EventDetailsFragment();
+            Fragment newFragment = new EventDetailsFragment();
             Bundle args = new Bundle();
             args.putInt(EventDetailsFragment.ARG_POSITION, position);
             newFragment.setArguments(args);
@@ -60,7 +142,6 @@ public class EventActivity extends AppCompatActivity implements EventListFragmen
 
             // Commit the transaction
             transaction.commit();
-        //}
-
+        }
     }
-}
+
